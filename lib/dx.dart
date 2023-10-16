2bd521b3a8c9117679256de07fbf99341f69f3af
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-10-16 18:53:17 +0100

    new update

--- lib/dx.dart
+++ lib/dx.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:demo_ville/data_table2.dart';
-import 'package:demo_ville/data_table_fixed.dart';
+import 'package:demo_ville/view/alertdailog.dart';
 import 'package:flutter/material.dart';
 
 class Dx extends StatefulWidget {
@@ -12,13 +14,62 @@ class Dx extends StatefulWidget {
 class _DxState extends State<Dx> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Manager"),
+        actions: [
+          GestureDetector(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadiusDirectional.circular(5)),
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Text(
+                "Info",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            onTap: () {
+              infoDialog(context);
+            },
+          ),
+          SizedBox(width: 10),
+          GestureDetector(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadiusDirectional.circular(5)),
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Text(
+                "Logout",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            onTap: () {},
+          ),
+          SizedBox(width: 10),
+        ],
+      ),
       body: SafeArea(
           child: Column(
         children: [
-          Text("hhhhh"),
-          Text("hhhhh"),
-          Text("hhhhh"),
+          Row(
+            children: [
+              SizedBox(width: 20),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [Text('To be reimbursed'), Text('\$100000')],
+              )),
+              Text('Filters'),
+              IconButton(
+                icon: Icon(Icons.filter_alt_sharp),
+                onPressed: () {
+                  filterDialog(context);
+                },
+              )
+            ],
+          ),
           Expanded(
             child: DataTable2Demo(),
           ),
